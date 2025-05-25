f552f75be2e114d2f9abded7f995da6e8af7ad47
Vittorio Malinverno <malinverno.vittorio@gmail.com>
2025-05-25 23:58:08 +0200

    the start of everything

---
+++ src/main/java/vittorio/tutorial/Tutorial.java
@@ -0,0 +1,19 @@
+package vittorio.tutorial;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Tutorial implements ModInitializer {
+
+	// The mod ID, used for identifying your mod in logs and other places
+	public static final String MOD_ID = "tutorial";
+
+	// Define a logger for this mod
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/vittorio/tutorial/TutorialClient.java
@@ -0,0 +1,10 @@
+package vittorio.tutorial;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialClient implements ClientModInitializer{
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
